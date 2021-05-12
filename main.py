fc1310071741c59d35d4777ed10583c5694495b3
kennmundell <64756644+kennmundell@users.noreply.github.com>
2021-05-12 12:37:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,8 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,7 +32,8 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        
+        scoreboard.increase_score()
+
 
 
 
